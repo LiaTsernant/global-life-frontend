7609f8bf51ec86ddf7c0886ab08a86adf902a397
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-10 11:20:38 -0700

    Moved to separate directory

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,25 +1,26 @@
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, AppRegistry } from 'react-native';
+import { NativeRouter, Route, Link } from "react-router-native";
 
-// export const Header = (props) => {
-//     return (
-//         <View style={styles.header}>
-//             <Text style={styles.text}>Global Life</Text>
-//         </View>
+export const Header = (props) => {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.text}>Global Life</Text>
+        </View>
       
-//     );
-// }
+    );
+};
 
-// const styles = StyleSheet.create({
-//     header: {
-//         height: 80,
-//         alignItems: 'center',
-//         justifyContent: 'flex-end',
-//         backgroundColor: 'black',
-//         paddingBottom: 10
-//     },
-//     text: {
-//         color: '#ffd05c',
-//         fontSize: 25
-//     }
-// })
+const styles = StyleSheet.create({
+    header: {
+        height: 80,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        backgroundColor: 'black',
+        paddingBottom: 10
+    },
+    text: {
+        color: '#ffd05c',
+        fontSize: 25
+    },
+});
