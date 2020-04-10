e286fb54107b235bd285bace55b6babb6924c59a
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-10 16:24:27 -0700

    Imported router

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Header } from './components/Header/Header';
+// import { Header } from './components/Header/Header';
+import Routes from './config/Routes';
+
+
+// import Login from './components/Login/Login';
+// import SignUp from './components/SignUp/SignUp';
+
+
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Header />
+      <View style={styles.backgroundContainer}>
+        {/* <Header /> */}
+        {/* <Login /> */}
+        {/* <SignUp /> */}
+        <Routes />
       </View>
     );
   };
 };
+
+const styles = StyleSheet.create({
+  backgroundContainer: {
+      backgroundColor: 'black',
+      flex: 1,
+      width: null,
+      height: null,
+      justifyContent: 'center',
+      alignItems: 'center',
+  }
+})
+
+
+// 8133376366 Kevin 
