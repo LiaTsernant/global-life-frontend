64521097fa6cdce8353110f3976dbca930ef8b4e
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-10 16:24:44 -0700

    Created routes

--- config/Routes.js
+++ config/Routes.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { Router, Stack, Scene } from 'react-native-router-flux';
+import Login from '../components/Login/Login';
+import SignUp from '../components/SignUp/SignUp';
+import Main from '../components/Main/Main';
+import About from '../components/About/About';
+import Profile from '../components/Profile/Profile';
 
-const Routes = () => {
+class Routes extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Stack key="root">
+                    <Scene key="login" component={ Login } title="Login"/>
+                    <Scene key="signup" component={ SignUp } title="SignUp"/>
+                    <Scene key="main" component={ Main } title="Main"/>
+                    <Scene key="about" component={ About } title="About"/>
+                    <Scene key="profile" component={ Profile } title="Profile"/>
+                </Stack>
+            </Router>
 
-}
+        )
+    };
+};
+
+export default Routes;
