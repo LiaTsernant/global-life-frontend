6a2a2a7ce39f42a103299802d5ec8ed8dd1de1a2
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-09 21:45:02 -0700

    Initial working version

---
+++ components/Header/Header.js
@@ -0,0 +1,25 @@
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export const Header = (props) => {
+//     return (
+//         <View style={styles.header}>
+//             <Text style={styles.text}>Global Life</Text>
+//         </View>
+      
+//     );
+// }
+
+// const styles = StyleSheet.create({
+//     header: {
+//         height: 80,
+//         alignItems: 'center',
+//         justifyContent: 'flex-end',
+//         backgroundColor: 'black',
+//         paddingBottom: 10
+//     },
+//     text: {
+//         color: '#ffd05c',
+//         fontSize: 25
+//     }
+// })
