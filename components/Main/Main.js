d76321b11788ab0357db18f9d7284691bb225c1f
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-10 18:20:22 -0700

    Added button About

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -1,27 +1,49 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-// import { Header } from './components/Header/Header';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
+import { Header } from '../../components/Header/Header';
+import { Actions } from 'react-native-router-flux';
+
+const { width: WIDTH } = Dimensions.get('window');
 
 export default class Main extends Component {
+  goToAbout = () => {
+    Actions.about()
+  }
+
   render() {
     return (
-      <View style={styles.backgroundContainer}>
+      <View>
+        <Header mainPage={'About'}/>
+
+        <View style={styles.backgroundContainer}>
           <Text>I am MAIN! YAAAAAAY!!!</Text>
+        </View>
+
+        <TouchableOpacity style={styles.btnAbout} onPress={this.goToAbout}>
+          <Text style={styles.text}>Login</Text>
+        </TouchableOpacity>
+
       </View>
+
     );
   };
 };
 
 const styles = StyleSheet.create({
   backgroundContainer: {
-      backgroundColor: 'black',
-      flex: 1,
-      width: null,
-      height: null,
-      justifyContent: 'center',
-      alignItems: 'center',
-  }
-})
-
-
-// 8133376366 Kevin 
+    flex: 1,
+    width: null,
+    height: null,
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+  },
+  btnAbout: {
+    width: WIDTH - 55,
+    height: 45,
+    borderRadius: 45,
+    backgroundColor: 'yellow',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+});
