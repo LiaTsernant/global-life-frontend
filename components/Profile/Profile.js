77115214276ee01bd1e846ea353f883f4670bd75
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-13 15:26:00 -0700

    Fixed logout login

--- components/Profile/Profile.js
+++ components/Profile/Profile.js
@@ -1,20 +1,53 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-// import { Header } from './components/Header/Header';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import  Header from '../../components/Header/Header';
+import { AsyncStorage } from 'react-native';
+import setAuthHeader from '../../utils/setAuthHeader';
+import { Actions } from 'react-native-router-flux';
+
 
 export default class Profile extends Component {
+  goToLogin(){
+    Actions.login()
+  };
+
+  logout = () => {
+    AsyncStorage.removeItem('user').
+      then(
+        this.goToLogin()
+      ).
+      catch(err => console.log(err))
+
+    // console.log(AsyncStorage.jwtToken); 
+    // setAuthHeader();
+    // this.setState({
+    //   email: '',
+    //   _id: ''
+    // });
+
+    // Actions.login();
+  };
+
   render() {
     return (
+      <>
+      <Header />
       <View style={styles.backgroundContainer}>
-          <Text>I AM Profile PAGE</Text>
+        <View>
+          <Text style={styles.countryName}>PROFILE</Text>
+        </View>
+
+        <TouchableOpacity onPress={ ()=>{ this.logout()} } >
+            <Text>Logout</Text>
+        </TouchableOpacity>
       </View>
+    </>
     );
   };
 };
 
 const styles = StyleSheet.create({
   backgroundContainer: {
-      backgroundColor: 'black',
       flex: 1,
       width: null,
       height: null,
