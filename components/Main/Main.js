059a3ec6e730b627c299659199d846dd5c9c8eec
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-12 21:12:41 -0700

    Created Main

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -1,30 +1,71 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
-import { Header } from '../../components/Header/Header';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions, Linking } from 'react-native';
+import Header from '../../components/Header/Header';
 import { Actions } from 'react-native-router-flux';
+import apiCalls from '../../api/apiCalls';
 
 const { width: WIDTH } = Dimensions.get('window');
 
 export default class Main extends Component {
+  state = {
+    data: ''
+  }
+
   goToAbout = () => {
     Actions.about()
   }
 
+  componentDidMount() {
+    console.log(this.state)
+    apiCalls.countryShow("5e93b59bc21195772283320e").
+      then(res => res.json()).
+      then((res) => {
+        this.setState({
+          data: res
+        });
+      }
+    );
+  }
+
   render() {
     return (
-      <View>
-        <Header mainPage={'About'}/>
-
-        <View style={styles.backgroundContainer}>
-          <Text>I am MAIN! YAAAAAAY!!!</Text>
+      <>
+      <Header scene={'main'} />
+      <View style={styles.backgroundContainer}>
+        <View>
+          <Text style={styles.countryName}>{this.state.data.name}</Text>
         </View>
 
-        <TouchableOpacity style={styles.btnAbout} onPress={this.goToAbout}>
-          <Text style={styles.text}>Login</Text>
+        <TouchableOpacity style={styles.button} onPress={ ()=>{ Linking.openURL(`tel:${this.state.data.emergency}`)} } >
+          <View style={styles.buttonText}>
+            <Text style={styles.label}>Emergency: </Text>
+            <Text style={styles.left}>{ this.state.data.emergency ? this.state.data.emergency : '' }</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={ ()=>{ Linking.openURL(`tel:${this.state.data.police}`)} } >
+          <View style={styles.buttonText}>
+            <Text style={styles.label}>Police: </Text>
+            <Text style={styles.left}>{ this.state.data.police ? this.state.data.police : '' }</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={ ()=>{ Linking.openURL(`tel:${this.state.data.firefighters}`)} } >
+          <View style={styles.buttonText}>
+            <Text style={styles.label}>Fire: </Text>
+            <Text style={styles.left}>{ this.state.data.firefighters ? this.state.data.firefighters : '' }</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={ ()=>{ Linking.openURL(`tel:${this.state.data.crisisHotline}`)} } >
+          <View style={styles.buttonText}>
+            <Text style={styles.label}>Crisis Hotline: </Text>
+            <Text>{ this.state.data.crisisHotline ? this.state.data.crisisHotline : '' }</Text>
+          </View>
         </TouchableOpacity>
 
       </View>
-
+      </>
     );
   };
 };
@@ -38,12 +79,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     color: 'white',
   },
-  btnAbout: {
+  button: {
     width: WIDTH - 55,
     height: 45,
     borderRadius: 45,
-    backgroundColor: 'yellow',
+    backgroundColor: 'red',
     justifyContent: 'center',
     marginTop: 20,
   },
+  buttonText: {
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    margin: 10
+  },
+  label: {
+    fontWeight: 'bold'
+  },
+  countryName: {
+    fontSize: 30
+  }
 });
