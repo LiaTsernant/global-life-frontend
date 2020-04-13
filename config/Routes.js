93cec0a05bbbf757f10a5618ceeb4df715550ba0
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-13 15:27:03 -0700

    Added route for modal

--- config/Routes.js
+++ config/Routes.js
@@ -5,6 +5,7 @@ import SignUp from '../components/SignUp/SignUp';
 import Main from '../components/Main/Main';
 import About from '../components/About/About';
 import Profile from '../components/Profile/Profile';
+import CountryModal from '../components/Modals/countryModal';
 
 class Routes extends React.Component {
     state = {
@@ -16,8 +17,9 @@ class Routes extends React.Component {
         return( 
             <Router>
                 <Scene key = "root">
+                {/* <Scene key="modal" component={ CountryModal } title="Modal" hideNavBar={true} initial={true}/> */}
+                <Scene key="login" component={ Login } title="Login" hideNavBar={true}/>
                     <Scene key="main" component={ Main } title="Main" hideNavBar={true} />
-                    <Scene key="login" component={ Login } title="Login" hideNavBar={true} initial={true}/>
                     <Scene key="signup" component={ SignUp } title="SignUp" hideNavBar={true}/>
                     <Scene key="about" component={ About } title="About" hideNavBar={true}/>
                     <Scene key="profile" component={ Profile } title="Profile" hideNavBar={true}/>
