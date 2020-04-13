e5972029c827a372814d0bd28b036a254f64221b
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-12 21:13:18 -0700

    Add default scenes

--- config/Routes.js
+++ config/Routes.js
@@ -6,18 +6,25 @@ import Main from '../components/Main/Main';
 import About from '../components/About/About';
 import Profile from '../components/Profile/Profile';
 
-const Routes = () => {
-    return( 
-        <Router>
-            <Scene key = "root">
-                <Scene key="main" component={ Main } title="Main" hideNavBar={true}/>
-                <Scene key="login" component={ Login } title="Login" initial={ true } hideNavBar={true}/>
-                <Scene key="signup" component={ SignUp } title="SignUp" hideNavBar={true}/>
-                <Scene key="about" component={ About } title="About" hideNavBar={true}/>
-                <Scene key="profile" component={ Profile } title="Profile" hideNavBar={true}/>
-            </Scene>
-        </Router>
-    );
+class Routes extends React.Component {
+    state = {
+        main: false,
+        login: false,
+    };
+
+    render() {
+        return( 
+            <Router>
+                <Scene key = "root">
+                    <Scene key="main" component={ Main } title="Main" hideNavBar={true} />
+                    <Scene key="login" component={ Login } title="Login" hideNavBar={true} initial={true}/>
+                    <Scene key="signup" component={ SignUp } title="SignUp" hideNavBar={true}/>
+                    <Scene key="about" component={ About } title="About" hideNavBar={true}/>
+                    <Scene key="profile" component={ Profile } title="Profile" hideNavBar={true}/>
+                </Scene>
+            </Router>
+        );
+    };
 };
 
 export default Routes;
