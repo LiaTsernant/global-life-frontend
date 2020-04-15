e9de3f77956c51caa2e31d82001e65c601f85394
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-14 17:59:14 -0700

    Changed country show

--- api/apiCalls.js
+++ api/apiCalls.js
@@ -3,37 +3,41 @@ const endpoint = 'http://192.168.8.189:4000/api/v1';
 
 // ------------------------ COUNTRY API CALLS ------------------------
 const countryIndex = () => {
-    return axios.get(endpoint + '/counties').
-        then(res => res).
-        catch(err => console.log(err));
+  return axios.get(endpoint + '/counties').
+    then(res => res).
+    catch(err => console.log(err));
 };
 
-const countryShow = (countryId) => {
-    return fetch(`${ endpoint }/countries/${ countryId }`).
-        catch(err => {
-            console.log(222)
-            console.log(err)
-        });
+const countryShow = (countryName) => {
+  return fetch(`${endpoint}/countries/${countryName}`).
+    catch(err => { console.log(err) });
 };
-
-// ------------------------ COUNTRY API CALLS ------------------------
+// ------------------------ AUTH API CALLS ------------------------
 const register = (user) => {
-    return axios.post(`${endpoint}/register`, user).
-        then(res => res).
-        catch(err => console.log(err));
+  return axios.post(`${endpoint}/register`, user).
+    then(res => res).
+    catch(err => console.log(err));
 };
 
 const login = (user) => {
-    return axios.post(`${endpoint}/login`, user).
-        then(res => res).
-        catch(err => console.log(err));
+  return axios.post(`${endpoint}/login`, user).
+    then(res => res).
+    catch(err => console.log(err));
 };
+// ------------------------ USER API CALLS --------------------------
+const userShow = (userId) => {
+  return fetch(`${endpoint}/users/${userId}`).
+    catch(err => { console.log(err) });
+
+}
+
 
 export default {
-    countryIndex,
-    countryShow,
-    register,
-    login
+  countryIndex,
+  countryShow,
+  register,
+  login,
+  userShow
 };
 
 
