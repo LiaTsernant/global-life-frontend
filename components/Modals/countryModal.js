f8fb6d195c52e9a57727b6f047a9651c56ac13a8
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-14 18:00:39 -0700

    Finished country modal

--- components/Modals/countryModal.js
+++ components/Modals/countryModal.js
@@ -1,58 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight, Modal } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, Modal, Dimensions } from 'react-native';
 import SimpleModal from './SimpleModal';
 
 class CountryModal extends React.Component {
-    state = {
-        isModalVisible: false,
-    }
+  state = {
+    isModalVisible: false,
+    country: '',
+  };
 
-    changeModalVisibility = (bool) => {
-        this.setState({
-            isModalVisible: bool
-        });
-    };
+  setData = (data) => {
+    this.setState({ country: data })
+    this.props.setSelectedCountry(data)
+  }
 
-    render() {
-        return (
-           <View style={styles.contentContainer}>
-              <Text style={styles.text}>
-                 {this.state.choosenData}
-              </Text>
-              <TouchableHighlight onPress={() => this.changeModalVisibility(true)} underlayColor={'#f1f1f1'} 
-                  style={[styles.touchableHighlight, {backgroundColor: 'orange'}]}>
-                 <Text style={styles.text}>Open Modal</Text>
-              </TouchableHighlight>  
-              <Modal transparent={true} animationType="fade" visible={this.state.isModalVisible} 
-                    onRequestClose={() => this.changeModalVisibility(false)} style={styles.modalContainer}>
-                  <SimpleModal changeModalVisibility={this.changeModalVisibility} setData={this.setData} />
-              </Modal>
-           </View>
-       )
-     }
+  changeModalVisibility = (bool) => {
+    this.setState({
+      isModalVisible: bool
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.contentContainer}>
+        <Text style={styles.text}>
+          {this.state.choosenData}
+        </Text>
+        <TouchableHighlight onPress={() => this.changeModalVisibility(true)} underlayColor={'#f1f1f1'}
+          style={[styles.touchableHighlight, { backgroundColor: 'orange' }]} >
+          <Text style={styles.text}>{this.state.country ? this.state.country : 'Click to Select a Country'}</Text>
+        </TouchableHighlight>  
+        <Modal transparent={true} animationType="fade" visible={this.state.isModalVisible}
+          onRequestClose={() => this.changeModalVisibility(false)} style={styles.modalContainer}>
+          <SimpleModal changeModalVisibility={this.changeModalVisibility} setData={this.setData} />
+        </Modal>
+      </View>
+    )
+  }
 };
 
+const { width: WIDTH } = Dimensions.get('window');
+
+
 const styles = StyleSheet.create({
-    contentContainer: {
-        flex: 1,
-        backgroundColor: 'yellow',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {
-        marginVertical: 20,
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    touchableHighlight: {
-        backgroundColor: 'white', 
-        alignSelf: 'stretch',
-        alignItems: 'center',
-    },
-    modalContainer: {
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
+  contentContainer: {
+  },
+  text: {
+    // marginVertical: 20,
+    // fontSize: 20,
+    // fontWeight: 'bold',
+  },
+  touchableHighlight: {
+    backgroundColor: 'white',
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    width: WIDTH - 55,
+    height: 40,
+    borderRadius: 45,
+    justifyContent: 'center'
+  },
+  modalContainer: {
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  }
 })
 
 export default CountryModal;
