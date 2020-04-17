7c443c04e2a697fb6776e78daa3ec32a98fd467d
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-04-16 19:24:03 -0700

    Finished Header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,66 +1,60 @@
 import React from 'react';
-import { 
-    StyleSheet, 
-    Text, 
-    View,
-    Dimensions 
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-import { Scene, Stack } from 'react-native-router-flux';
-
-
-const { width: WIDTH } = Dimensions.get('window');
 
 class Header extends React.Component {
+  goToAbout = () => {
+    Actions.about()
+  };
 
-    goToAbout = () => {
-        Actions.about()
+  goToProfile = () => {
+    Actions.profile()
+  };
+
+  goToMain = () => {
+    Actions.main()
+  };
+
+  render() {
+    if (this.props.scene === 'main') {
+      return (
+        <View style={styles.header}>
+          <Text style={styles.text} onPress={() => this.goToAbout()}>About</Text>
+          <Text style={styles.text} onPress={() => this.goToProfile()}>Profile</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.header}>
+          <Text style={styles.text} onPress={() => this.goToMain()}>Main</Text>
+          <Text style={styles.text} onPress={() => this.goToProfile()}>Profile</Text>
+        </View>
+      );
     };
-
-    goToProfile = () => {
-        Actions.profile()
-    };
-
-    goToMain = () => {
-        Actions.main()
-    }
-
-    render() {
-        console.log('re-render HEADER')
-        if (this.props.scene === 'main') {
-            return (
-                <View style={styles.header}>
-                    <Text style={styles.text} onPress={ () => this.goToAbout() }>About</Text>
-                    <Text style={styles.text} onPress={ () => this.goToProfile() }>Profile</Text>
-                </View>
-            );
-        } else {
-            return (
-                <View style={styles.header}>
-                    <Text style={styles.text} onPress={ () => this.goToMain() }>Main</Text>
-                    <Text style={styles.text} onPress={ () => this.goToProfile() }>Profile</Text>
-                </View>
-            );
-        };
-    };
+  };
 };
-
+const { width: WIDTH } = Dimensions.get('window');
 const styles = StyleSheet.create({
-    header: {
-        width: WIDTH,
-        height: 50,
-        alignItems: 'center',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        backgroundColor: 'black',
-        paddingBottom: 10
-    },
-    text: {
-        color: 'white',
-        fontSize: 20,
-        margin: 10,
-        marginTop: 30
-    },
+  header: {
+    width: WIDTH,
+    height: 50,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'black',
+    paddingBottom: 10
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+    margin: 10,
+    marginTop: 30
+  },
 });
 
 export default Header;
